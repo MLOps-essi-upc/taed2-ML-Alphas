f7979ed7025b5dc615db1221e3eabc4e3364bf8b
Eduard Aliaga <eduard.ramon.aliaga@estudiantat.upc.edu>
2023-10-23 15:59:46 +0200

    corrected version of prepare.py

--- notebooks/prepare.py
+++ notebooks/prepare.py
@@ -14,32 +14,41 @@ import pandas as pd
 from tensorflow.keras import layers
 from pathlib import Path
 
-
-
-
-
-def prepare_data(dataset,transform):
+def prepare_data(dataset, data_augmentation):
     dataset2 = [[0,''] for i in range(len(dataset)*2)]
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),  # Resize the image to (224, 224)
+        transforms.ToTensor(),          # Convert PIL Image to tensor
+    ])
 
-    for i in range(0,len(dataset)):
-        image_bytes = dataset.iloc[i]['image']['bytes'] # Get bytes
-        image_pil = Image.open(io.BytesIO(image_bytes)) # Convert bytes to PIL Image
-        aug_image = tf.image.stateless_random_brightness(image_pil, max_delta=np.random.rand(), seed=(3,0))
-        pil_format = Image.fromarray(aug_image.numpy())
-        dataset2[2*i] = [image_pil,dataset.iloc[i]['label']]
-        dataset2[2*i+1] = [pil_format,dataset.iloc[i]['label']]
-        print("hola")
-    return dataset2
+    if data_augmentation:
+        for i in range(0,len(dataset)):
+            image_bytes = dataset.iloc[i]['image']['bytes'] # Get bytes
+            image_pil = Image.open(io.BytesIO(image_bytes)) # Convert bytes to PIL Image
+            aug_image = tf.image.stateless_random_brightness(image_pil, max_delta=np.random.rand(), seed=(3,0))
+            pil_format = Image.fromarray(aug_image.numpy())
+            dataset2[2*i] = [transform(image_pil),dataset.iloc[i]['label']]
+            dataset2[2*i+1] = [transform(pil_format),dataset.iloc[i]['label']]
+        df = pd.DataFrame(dataset2)
+        return df
+    else:
+        for i in range(0,len(dataset)):
+            image_bytes = dataset.iloc[i]['image']['bytes'] # Get bytes
+            image_pil = Image.open(io.BytesIO(image_bytes)) # Convert bytes to PIL Image
+            dataset2[i] = [transform(image_pil),dataset.iloc[i]['label']]
+        df = pd.DataFrame(dataset2)
+        return dataset2
 
 
 def main(repo_path):
-    data_path = repo_path / "data"
-    train_path = data_path / "raw/train"
-    test_path = data_path / "raw/val"
-    train_files, train_labels = prepare_data(train_path)
-    test_files, test_labels = prepare_data(test_path)
-    prepared = data_path / "prepared"
-    train_files.to_csv(prepared/"train.csv")
-    test_files.to_csv(prepared/"test.csv")
+    data_augmentation=False
+    data_path = repo_path + "data"
+    train_path = data_path + "raw/train"
+    test_path = data_path + "raw/val"
+    train_files = prepare_data(train_path, data_augmentation)
+    test_files = prepare_data(test_path, data_augmentation)
+    prepared = data_path + "prepared"
+    train_files.to_csv(prepared + "/train.csv")
+    test_files.to_csv(prepared + "/test.csv")
 
 main("/Users/aliag/taed-ML-Alphas/")
