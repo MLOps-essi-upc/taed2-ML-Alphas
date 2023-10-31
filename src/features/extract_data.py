9c972ab2d2aa852b2e367e6da12b221d32cbe7c1
“Adriana” <adriana.diaz@estudiantat.upc.edu>
2023-10-31 22:04:47 +0100

    Add coments

--- src/features/extract_data.py
+++ src/features/extract_data.py
@@ -4,10 +4,10 @@ from datasets import load_dataset
 train_dataset = load_dataset('Falah/Alzheimer_MRI', split='train')
 test_dataset = load_dataset('Falah/Alzheimer_MRI', split='test')
 
-# Set path
+# Set output path for raw data
 raw_train_path = 'data/raw_data/train'
 raw_test_path = 'data/raw_data/test'
 
-# Store data in .parquet format
+# Store data in parquet format
 train_dataset.to_parquet(raw_train_path+'/train.parquet')
 test_dataset.to_parquet(raw_test_path+ '/test.parquet')
